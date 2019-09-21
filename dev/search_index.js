e97e42e5c410bf67bbd5965e0ea8f6cebf599bd1
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-09-21 08:27:45 +0000

    build based on 19047e5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SpectralLibraries.jl-1","page":"Home","title":"SpectralLibraries.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [SpectralLibraries]","category":"page"}]
+}
