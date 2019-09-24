40d407606c1eb9f20aec5f5e400a627d34dac3aa
Miles Lucas <mdlucas@hawaii.edu>
2019-09-24 00:46:32 -1000

    fix prettyurls boolean error

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,18 @@
 using Documenter, SpectralLibraries
 
 makedocs(;
-    modules=[SpectralLibraries],
-    format=Documenter.HTML(
-        prettyurls = get(ENV, "CI", false)
-    ),
-    pages=[
+    modules = [SpectralLibraries],
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    pages = [
         "Home" => "index.md",
         "Libraries" => [
             "PHOENIX ACES" => "phoenix.md",
         ],
     ],
-    strict=true,
-    repo="https://github.com/mileslucas/SpectralLibraries.jl/blob/{commit}{path}#L{line}",
-    sitename="SpectralLibraries.jl",
-    authors="Miles Lucas <mdlucas@hawaii.edu>",
-)
+    strict = true,
+    repo = "https://github.com/mileslucas/SpectralLibraries.jl/blob/{commit}{path}#L{line}",
+    sitename = "SpectralLibraries.jl",
+    authors = "Miles Lucas <mdlucas@hawaii.edu>",)
 
-deploydocs(;
-    repo="github.com/mileslucas/SpectralLibraries.jl",
-)
+deploydocs(
+    repo = "github.com/mileslucas/SpectralLibraries.jl",)
