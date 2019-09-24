ffeda2799e6cf4ed2d7d1ff491d87f12c2fba028
Miles Lucas <mdlucas@hawaii.edu>
2019-09-24 12:29:15 -1000

    add checksum to datadep

--- src/SpectralLibraries.jl
+++ src/SpectralLibraries.jl
@@ -10,7 +10,8 @@ function __init__()
     using DataDeps
     register(DataDep("PHOENIX_MODELS",
         "Download master PHOENIX ACES models file",
-        "https://github.com/mileslucas/SpectralLibraries.jl/raw/master/data/PHOENIX_MODELS.csv"))
+        "https://github.com/mileslucas/SpectralLibraries.jl/raw/master/data/PHOENIX_MODELS.csv",
+        "8cebcb86999e90d2d2d7c49381574ba1b97edcd784a43cf87772e058e3fbe177"))
 end
 
 end # module
